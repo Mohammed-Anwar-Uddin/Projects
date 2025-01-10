c9c172e623edd26a69c664e51a2e8d4db7a54e93
Mohammed-Anwar-Uddin <mohammedmuzaffar0412@gmail.com>
2025-01-10 23:05:38 +0530

    add movie person and tv Details component and redux store is create

---
+++ movie_app/src/store/reducers/movieSlice.jsx
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState = {
+    info:null
+}
+
+export const counterSlice = createSlice({
+    name:"movie",
+    initialState,
+    reducers:{}
+})
+
+
+
+export default counterSlice.reducer
